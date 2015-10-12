b6fdab60aa41082e5cab6d5b938472fa58720ae0
supercaracal <supercaracal@yahoo.co.jp>
2015-10-12 23:39:30 +0900

    feature test refactoring

--- spec/features/home_controller_spec.rb
+++ spec/features/home_controller_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 feature 'Home' do
   given(:footer_content) { 'PocaPocaUnty' }
+  background { visit root_path }
 
   scenario 'TOPにアクセスすると200が返る' do
-    visit root_path
     expect(page.status_code).to eq(200)
   end
 
   scenario 'TOPにアクセスするとコンテンツが表示される' do
-    visit root_path
     expect(page).to have_content(footer_content)
   end
 end

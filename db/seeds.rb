1ff1eab419863ee559fec2e070bc91a8bf464d36
supercaracal <supercaracal@yahoo.co.jp>
2016-04-03 18:34:28 +0900

    2016-2Q added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 # Mayor.create(name: 'Emanuel', city: cities.first)
 
 _ch1 = Channel.create(name: 'NHK総合', code: '1ch')
-_ch2 = Channel.create(name: 'Eテレ', code: '2ch')
+ch2 = Channel.create(name: 'Eテレ', code: '2ch')
 ch4 = Channel.create(name: '日本テレビ', code: '4ch')
-ch5 = Channel.create(name: 'テレビ朝日', code: '5ch')
+_ch5 = Channel.create(name: 'テレビ朝日', code: '5ch')
 ch6 = Channel.create(name: 'TBSテレビ', code: '6ch')
 ch7 = Channel.create(name: 'テレビ東京', code: '7ch')
 ch8 = Channel.create(name: 'フジテレビ', code: '8ch')
 ch9 = Channel.create(name: 'TOKYO MX', code: '9ch')
 _ch3 = Channel.create(name: 'テレ玉', code: '10ch')
 _ch103 = Channel.create(name: 'BSプレミアム', code: '103')
-ch161 = Channel.create(name: 'BS-TBS', code: '161')
+_ch161 = Channel.create(name: 'BS-TBS', code: '161')
 _ch181 = Channel.create(name: 'BSフジ', code: '181')
 _ch211 = Channel.create(name: 'BS11', code: '211')
 
@@ -31,32 +31,42 @@ sun = DayOfWeek.create(name: '日', code: 'sun')
 Lineup.create([
   { title: 'くまみこ', start_time: '00:30:00', channel: ch9, day_of_week: mon },
   { title: 'Re:ゼロから始める異世界生活', start_time: '01:30:00', channel: ch7, day_of_week: mon },
-  { title: '最弱無敗の神装機竜', start_time: '00:30:00', channel: ch9, day_of_week: tue },
-  { title: '銀魂', start_time: '18:00:00', channel: ch7, day_of_week: wed },
-  { title: '無彩限のファントム・ワールド', start_time: '00:00:00', channel: ch9, day_of_week: thu },
-  { title: 'この素晴らしい世界に祝福を！', start_time: '01:00:00', channel: ch9, day_of_week: thu },
-  { title: 'ハルチカ ハルタとチカは青春する', start_time: '01:30:00', channel: ch9, day_of_week: thu },
+  { title: 'ばくおん!!', start_time: '00:00:00', channel: ch9, day_of_week: tue },
+  { title: 'パンでPeace!', start_time: '01:00:00', channel: ch9, day_of_week: tue },
+  { title: 'うさかめ', start_time: '01:05:00', channel: ch9, day_of_week: tue },
+  { title: '聖戦ケルベロス 竜刻のファタリテ', start_time: '01:30:00', channel: ch9, day_of_week: tue },
+  { title: 'ハンドレッド', start_time: '02:00:00', channel: ch9, day_of_week: tue },
+  { title: 'ジョーカー・ゲーム', start_time: '00:30:00', channel: ch9, day_of_week: wed },
+  { title: '双星の陰陽師', start_time: '18:30:00', channel: ch7, day_of_week: wed },
+  { title: '鬼斬', start_time: '01:00:00', channel: ch9, day_of_week: thu },
+  { title: '文豪ストレイドッグス', start_time: '01:05:00', channel: ch9, day_of_week: thu },
   { title: 'ポケモン', start_time: '19:00:00', channel: ch7, day_of_week: thu },
   { title: 'NARUTO疾風伝', start_time: '19:30:00', channel: ch7, day_of_week: thu },
-  { title: '少女たちは荒野を目指す', start_time: '22:30:00', channel: ch9, day_of_week: thu },
-  { title: 'ルパン三世', start_time: '02:00:00', channel: ch4, day_of_week: fri },
-  { title: '僕だけがいない街', start_time: '02:00:00', channel: ch8, day_of_week: fri },
-  { title: '暗殺教室', start_time: '02:30:00', channel: ch8, day_of_week: fri },
-  { title: 'ディバインゲート', start_time: '22:30:00', channel: ch9, day_of_week: fri },
+  { title: 'クロムクロ', start_time: '22:00:00', channel: ch9, day_of_week: thu },
+  { title: 'あんハピ♪', start_time: '22:30:00', channel: ch9, day_of_week: thu },
+  { title: 'ネトゲの嫁は女の子じゃないと思った？', start_time: '23:30:00', channel: ch9, day_of_week: thu },
+  { title: '甲鉄城のカバネリ', start_time: '02:30:00', channel: ch8, day_of_week: fri },
+  { title: '暗殺教室', start_time: '03:00:00', channel: ch8, day_of_week: fri },
+  { title: '少年メイド', start_time: '04:00:00', channel: ch6, day_of_week: fri },
+  { title: '坂本ですが？', start_time: '04:30:00', channel: ch6, day_of_week: fri },
+  { title: 'うしおととら', start_time: '22:30:00', channel: ch9, day_of_week: fri },
   { title: 'ウルトラスーパーアニメタイム', start_time: '23:00:00', channel: ch9, day_of_week: fri },
   { title: 'ジョジョの奇妙な冒険 ダイヤモンドは砕けない', start_time: '00:30:00', channel: ch9, day_of_week: sat },
   { title: 'テラフォーマーズ リベンジ', start_time: '01:00:00', channel: ch9, day_of_week: sat },
-  { title: '亜人', start_time: '02:00:00', channel: ch6, day_of_week: sat },
-  { title: '昭和元禄落語心中', start_time: '02:30:00', channel: ch6, day_of_week: sat },
+  { title: '迷い家', start_time: '02:00:00', channel: ch6, day_of_week: sat },
+  { title: '亜人', start_time: '02:30:00', channel: ch6, day_of_week: sat },
+  { title: '境界のRINNE2', start_time: '17:30:00', channel: ch2, day_of_week: sat },
   { title: '逆転裁判', start_time: '17:30:00', channel: ch4, day_of_week: sat },
+  { title: '田中くんはいつもけだるげ', start_time: '22:00:00', channel: ch9, day_of_week: sat },
   { title: 'キズナイーバー', start_time: '23:30:00', channel: ch9, day_of_week: sat },
+  { title: 'はいふり', start_time: '00:00:00', channel: ch9, day_of_week: sun },
   { title: '学園都市アスタリスク', start_time: '00:30:00', channel: ch9, day_of_week: sun },
-  { title: 'だがしかし', start_time: '01:30:00', channel: ch161, day_of_week: sun },
+  { title: 'ふらいんぐうぃっち', start_time: '02:30:00', channel: ch4, day_of_week: sun },
   { title: 'Re:ゼロから始める異世界体験', start_time: '02:00:00', channel: ch7, day_of_week: sun },
-  { title: 'ワールドトリガー', start_time: '06:30:00', channel: ch5, day_of_week: sun },
   { title: 'ドラゴンボール超', start_time: '09:00:00', channel: ch8, day_of_week: sun },
   { title: 'ワンピース', start_time: '09:30:00', channel: ch8, day_of_week: sun },
   { title: '僕のヒーローアカデミア', start_time: '17:00:00', channel: ch6, day_of_week: sun },
   { title: 'マクロス Δ', start_time: '22:30:00', channel: ch9, day_of_week: sun },
+  { title: 'コンクリート・レボルティオ～超人幻想～THE LAST SONG', start_time: '23:00:00', channel: ch9, day_of_week: sun },
   { title: 'SuperGT', start_time: '23:30:00', channel: ch7, day_of_week: sun }
 ])

11529e5f3441fb468156c8d706cea985a2699839
supercaracal <supercaracal@yahoo.co.jp>
2016-07-18 20:44:54 +0900

    2016-3Q triage

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,6 @@ sun = DayOfWeek.create(name: '日', code: 'sun')
 
 Lineup.create(
   [
-    { title: 'あまんちゅ!', start_time: '00:30:00', channel: ch9, day_of_week: mon },
     { title: 'Re:ゼロから始める異世界生活', start_time: '01:30:00', channel: ch7, day_of_week: mon },
     { title: '斉木楠雄のΨ難', start_time: '02:00:00', channel: ch7, day_of_week: mon },
     { title: 'ダンガンロンパ3 未来編', start_time: '23:00:00', channel: ch9, day_of_week: mon },
@@ -46,20 +45,14 @@ Lineup.create(
     { title: 'ポケモン', start_time: '19:00:00', channel: ch7, day_of_week: thu },
     { title: 'NARUTO疾風伝', start_time: '19:30:00', channel: ch7, day_of_week: thu },
     { title: 'クロムクロ', start_time: '22:00:00', channel: ch9, day_of_week: thu },
-    { title: 'レガリア', start_time: '22:30:00', channel: ch9, day_of_week: thu },
     { title: 'ダンガンロンパ3 絶望編', start_time: '23:30:00', channel: ch9, day_of_week: thu },
-    { title: 'はんだくん', start_time: '02:00:00', channel: ch6, day_of_week: fri },
-    { title: 'この美術部には問題がある!', start_time: '02:30:00', channel: ch6, day_of_week: fri },
     { title: 'ReLIFE', start_time: '00:00:00', channel: ch9, day_of_week: sat },
     { title: 'ジョジョの奇妙な冒険 ダイヤモンドは砕けない', start_time: '00:30:00', channel: ch9, day_of_week: sat },
-    { title: 'ねじ巻き精霊戦記', start_time: '01:00:00', channel: ch9, day_of_week: sat },
     { title: '91days', start_time: '02:00:00', channel: ch6, day_of_week: sat },
     { title: 'ベルセルク', start_time: '02:30:00', channel: ch6, day_of_week: sat },
     { title: '境界のRINNE2', start_time: '17:30:00', channel: ch2, day_of_week: sat },
     { title: '食戟のソーマ 弐ノ皿', start_time: '22:00:00', channel: ch9, day_of_week: sat },
     { title: 'Rewrite', start_time: '23:30:00', channel: ch9, day_of_week: sat },
-    { title: 'クオリディア・コード', start_time: '00:00:00', channel: ch9, day_of_week: sun },
-    { title: 'アンジュ・ヴィエルジュ', start_time: '01:30:00', channel: ch9, day_of_week: sun },
     { title: 'ドラゴンボール超', start_time: '09:00:00', channel: ch8, day_of_week: sun },
     { title: 'ワンピース', start_time: '09:30:00', channel: ch8, day_of_week: sun },
     { title: 'アルスラーン戦記 風塵乱舞', start_time: '17:00:00', channel: ch6, day_of_week: sun },

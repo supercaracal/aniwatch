ea9e09a66979da6639c789fa55724c9452d4278d
Taishi Kasuga <kasuga@livesense.co.jp>
2016-08-09 15:35:56 +0900

    bootstrap v4 update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year(now: Time.zone.now)
+    now.year
+  end
+
+  def current_quarter(now: Time.zone.now)
+    now.month / 3 + ((now.month % 3).zero? ? 0 : 1)
+  end
 end

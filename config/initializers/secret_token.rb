8ae554e314360aa2659e6242989d4b44e19d56f8
supercaracal <supercaracal@yahoo.co.jp>
2017-04-30 19:46:11 +0900

    fix some issues

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Aniwatch2::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+Aniwatch::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
--- generate_github_io_page.sh
+++ generate_github_io_page.sh
@@ -5,8 +5,10 @@ DISABLE_DATABASE_ENVIRONMENT_CHECK=1 RAILS_ENV=production bin/rails db:setup
 mkdir -p docs
 rm docs/*
 
-fetch_html="Rails.logger.level = 5; Rails.application.call('REQUEST_METHOD' => 'GET', 'rack.input' => '').third.each { |b| puts b }.first"
-RAILS_ENV=production SECRET_KEY_BASE=$(bin/rails secret) bin/rails runner "$fetch_html" | sed -e 's#/assets/#/aniwatch/#g' > docs/index.html
+disable_log='Rails.logger.level = 5'
+fetch_html='Rails.application.call("REQUEST_METHOD" => "GET", "rack.input" => "").third.each { |b| puts b }'
+rails_cmd="${disable_log}; ${fetch_html}"
+SECRET_KEY_BASE=$(bin/rails secret) bin/rails runner -e production "$rails_cmd" | sed -e 's#/assets/#/aniwatch/#g' > docs/index.html
 
 bin/rails assets:precompile
 mv public/assets/* docs/

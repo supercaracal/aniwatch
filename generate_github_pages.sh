e1ed894b4bd663e026e25687e898654ddfa5897a
supercaracal <supercaracal@yahoo.co.jp>
2017-05-02 00:48:26 +0900

    [ci skip] refactor a shellscript

--- generate_github_pages.sh
+++ generate_github_pages.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 RAILS_ENV=production bin/rails db:setup
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+export RAILS_ENV=production
+export SECRET_KEY_BASE=$(bin/rails secret)
+
+readonly DISABLE_LOG='Rails.logger.level = 5'
+readonly FETCH_HTML='Rails.application.call("REQUEST_METHOD" => "GET", "rack.input" => "").third.each { |b| puts b }'
+
+bin/rails db:setup
 
 mkdir -p docs
 rm docs/*
 
-RAILS_ENV=production bin/rails assets:precompile
+bin/rails assets:precompile
 mv public/assets/* docs/
 
-disable_log='Rails.logger.level = 5'
-fetch_html='Rails.application.call("REQUEST_METHOD" => "GET", "rack.input" => "").third.each { |b| puts b }'
-rails_cmd="${disable_log}; ${fetch_html}"
-SECRET_KEY_BASE=$(bin/rails secret) bin/rails runner -e production "$rails_cmd" | sed -e 's#/assets/#/aniwatch/#g' > docs/index.html
+rails_cmd="${DISABLE_LOG}; ${FETCH_HTML}"
+bin/rails runner -e production "$rails_cmd" | sed -e 's#/assets/#/aniwatch/#g' > docs/index.html

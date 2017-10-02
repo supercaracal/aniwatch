d00fe1299b2436cd3ca3a82e036dcdcaaad4aff6
Taishi Kasuga <proxy0721@gmail.com>
2017-10-02 18:28:32 +0900

    2017-4Q first impression

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,6 +32,8 @@ sun = DayOfWeek.create(name: '日', code: 'sun')
 
 Lineup.create(
   [
+    { title: '干物妹!うまるちゃんR', start_time: '00:00:00', channel: ch9, day_of_week: mon },
+    { title: '血界戦線&BEYOND', start_time: '00:30:00', channel: ch9, day_of_week: mon },
     { title: '銀魂', start_time: '02:00:00', channel: ch7, day_of_week: mon },
     { title: 'UQ HOLDER', start_time: '00:00:00', channel: ch9, day_of_week: tue },
     { title: 'ディアホライゾン', start_time: '01:00:00', channel: ch9, day_of_week: tue },

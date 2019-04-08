ade9a3e7a508d034bfca2a677a823487de0177b2
Taishi Kasuga <proxy0721@gmail.com>
2019-04-08 22:44:20 +0900

    2019-2Q triage

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,6 @@ sun = DayOfWeek.create(name: '日', code: 'sun')
 Lineup.create(
   [
     { title: 'Fairy gone', start_time: '00:00:00', channel: ch9, day_of_week: mon },
-    { title: '八月のシンデレラナイン', start_time: '01:30:00', channel: ch7, day_of_week: mon },
     { title: '八十亀ちゃん', start_time: '21:54:00', channel: ch9, day_of_week: mon },
     { title: 'どろろ', start_time: '22:00:00', channel: ch9, day_of_week: mon },
     { title: 'ブラッククローバー', start_time: '18:30:00', channel: ch7, day_of_week: tue },
